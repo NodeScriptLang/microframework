cae3e96811443010a5122d12589640e6850a624b
inca <boris@okunskiy.name>
2024-04-07 20:12:16 +0300

    chore: add default bindings and ProcessMetrics

--- src/main/BaseApp.ts
+++ src/main/BaseApp.ts
@@ -3,7 +3,11 @@ import 'reflect-metadata';
 import { InitializationError } from '@nodescript/errors';
 import { Logger } from '@nodescript/logger';
 import dotenv from 'dotenv';
+import { Config, ProcessEnvConfig } from 'mesh-config';
 import { dep, Mesh } from 'mesh-ioc';
+
+import { ProcessMetrics } from './ProcessMetrics.js';
+import { StandardLogger } from './StandardLogger.js';
 
 const process = global.process;
 
@@ -16,6 +20,9 @@ export abstract class BaseApp {
 
     constructor(readonly mesh: Mesh) {
         this.mesh.connect(this);
+        this.mesh.service(Config, ProcessEnvConfig);
+        this.mesh.service(Logger, StandardLogger);
+        this.mesh.service(ProcessMetrics);
     }
 
     /**
