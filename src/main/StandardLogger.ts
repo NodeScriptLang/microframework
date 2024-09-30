0e627685a8a4d05cc2e1f875257ecc88118c4b5a
inca <boris@okunskiy.name>
2024-09-30 15:22:48 +0300

    chore: add LOG_STACK option

--- src/main/StandardLogger.ts
+++ src/main/StandardLogger.ts
@@ -5,11 +5,21 @@ export class StandardLogger extends ConsoleLogger {
 
     @config({ default: 'info' }) LOG_LEVEL!: string;
     @config({ default: false }) LOG_PRETTY!: boolean;
+    @config({ default: false }) LOG_STACK!: boolean;
 
     constructor() {
         super();
-        this.formatter = this.LOG_PRETTY ? new DefaultLogFormatter() : new StructuredLogFormatter();
+        this.formatter = this.createFormatter();
         this.setLevel(this.LOG_LEVEL);
+    }
+
+    private createFormatter() {
+        if (this.LOG_PRETTY) {
+            return new DefaultLogFormatter();
+        }
+        const fmt = new StructuredLogFormatter();
+        fmt.includeStack = this.LOG_STACK;
+        return fmt;
     }
 
 }
